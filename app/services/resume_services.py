aaee2d369abd8238bdf9c30722d4a0db4c190c18
Po-Yao Huang <phua4038@uni.sydney.edu.au>
2025-12-02 10:47:35 +1100

    doc: add examples

--- app/services/resume_services.py
+++ app/services/resume_services.py
@@ -1,3 +1,6 @@
 # TODO: sample usage
 # def upload_resume(db: Session = Depends(get_db)):
 #     return upload_resume(db)
+
+# def get_resume(db: Session = Depends(get_db)):
+#     return get_resume(db)
